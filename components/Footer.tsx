c746e3adb72741df4c58b4c9814374e12579202e
Yuhei Okazaki <okazaki@fusic.co.jp>
2021-01-18 07:14:41 +0900

    Fix footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,10 +6,10 @@ export const Footer: React.FC = () => {
     <footer className="bg-gray-800 body-font">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <p className="text-sm text-white sm:py-2 sm:mt-0 mt-4">
+          <p className="text-sm text-white sm:py-2">
             Copyright © 2021 yuuu. All Rights Reserved.
           </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <span className="inline-flex sm:ml-auto justify-center sm:justify-start">
             <Link href="/admin/signin">
               <a className="ml-3 text-sm text-white">Only administrator</a>
             </Link>
