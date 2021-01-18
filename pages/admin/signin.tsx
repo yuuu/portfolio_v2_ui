eb3ded620131fa2f093dfffe501f54d87437b38a
Yuhei Okazaki <okazaki@fusic.co.jp>
2021-01-19 05:21:06 +0900

    Fix layout.

--- pages/admin/signin.tsx
+++ pages/admin/signin.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import axiosBase from 'axios'
+
+const axios = axiosBase.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+  withCredentials: true,
+})
 
 const Signin: React.FC = () => {
+  const { register, handleSubmit, errors } = useForm()
+  const onSubmit = async (data) => {
+    await axios.post('/administrators/sign_in', { administrator: data })
+    await axios.get('/me')
+  }
+
   return (
     <>
       <div className="flex justify-center px-4 sm:px-6 lg:px-8 pt-8">
@@ -16,7 +33,11 @@ const Signin: React.FC = () => {
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600"></p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form
+            className="mt-8 space-y-6"
+            action="#"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <input type="hidden" name="remember" value="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -31,7 +52,9 @@ const Signin: React.FC = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  ref={register({ required: true })}
                 />
+                {errors.emailRequired && <span>This field is required</span>}
               </div>
               <div>
                 <label htmlFor="password" className="sr-only">
@@ -45,7 +68,9 @@ const Signin: React.FC = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  ref={register({ required: true })}
                 />
+                {errors.passwordRequired && <span>This field is required</span>}
               </div>
             </div>
 
@@ -78,7 +103,7 @@ const Signin: React.FC = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <svg
