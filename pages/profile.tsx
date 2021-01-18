fe597ccc5c0bb23f029edac3b8c2d7f21bff1340
Yuhei Okazaki <okazaki@fusic.co.jp>
2021-01-19 04:25:49 +0900

    Fix image size in profile.

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -19,7 +19,10 @@ const Profile: React.FC = () => {
       <Header title="Profile" />
       <div className="flex flex-col md:flex-row space-x-4">
         <div className="flex-none flex justify-center">
-          <img src="/profile.png" className="max-h-96 max-w-sm mb-4 shadow" />
+          <img
+            src="/profile.png"
+            className="max-h-80 max-w-xs md:max-h-96 md:max-w-sm mb-4 shadow"
+          />
         </div>
         <div className="flex-grow">
           <h1 className="text-4xl leading-tight text-gray-900 pb-2 mb-4 border-b-2">
