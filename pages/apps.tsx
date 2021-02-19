d13bd181adabfb96a83e378e126fb82f29f4bfa6
Yuhei Okazaki <okazaki@fusic.co.jp>
2021-02-20 07:23:47 +0900

    Remove comment

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -28,57 +28,6 @@ const Apps: NextPage<{ apps: App[]; generatedAt: string }> = ({
 }) => {
   const { currentUser } = useAuth()
 
-  // const apps = [
-  //   {
-  //     id: 1,
-  //     category: 'Web System',
-  //     title: "yuuu's portfolio",
-  //     description: 'このページです。Next.js + Ruby on Railsで構築しています。',
-  //     link: '/',
-  //     image: '/images/apps/portfolio_v2.png',
-  //   },
-  //   {
-  //     id: 2,
-  //     category: 'Web System',
-  //     title: "yuuu's portfolio (old)",
-  //     description: '旧ポートフォリオです。Nuxt.jsで作成しました。',
-  //     link: 'https://portfolio.y-uuu.net/',
-  //     image: '/images/apps/portfolio.png',
-  //   },
-  //   {
-  //     id: 3,
-  //     category: 'Web System',
-  //     title: 'C2Diary',
-  //     description: '自分専用の日記システムです。',
-  //     link: 'https://c2diary.herokuapp.com/',
-  //     image: '/images/apps/c2diary.png',
-  //   },
-  //   {
-  //     id: 4,
-  //     category: 'Slack Bot',
-  //     title: 'Tigers Eye',
-  //     description: '阪神タイガースの試合状況をSlackへ通知するアプリです。',
-  //     link: 'https://github.com/yuuu/tigers-eye',
-  //     image: '/images/apps/tigers-eye.png',
-  //   },
-  //   {
-  //     id: 5,
-  //     category: 'Plugin',
-  //     title: 'Redmine Nikoca Re Plugin',
-  //     description: 'Redmineにニコカレ機能を追加するためのプラグインです。',
-  //     link: 'https://github.com/yuuu/redmine_nikoca_re',
-  //     image: '/images/apps/nikokare.png',
-  //   },
-  //   {
-  //     id: 6,
-  //     category: 'Desktop App',
-  //     title: 'Minamoni',
-  //     description: 'ETロボコン用に製作したモニタリング用のツールです。',
-  //     link: 'https://github.com/yuuu/Minamoni',
-  //     image: '/images/apps/minamoni.png',
-  //   },
-  // ]
-
   return (
     <Layout signedin={!!currentUser} loading={!apps} generatedAt={generatedAt}>
       <Header title="Apps" />
